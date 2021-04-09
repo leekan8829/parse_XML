93827b79ec80deaf1b930a4e70ab3e0e04d099f1
leekan8829 <40474568+leekan8829@users.noreply.github.com>
2021-04-09 15:44:12 +0800

    Update find_DR_VER2.py

--- find_DR_VER2.py
+++ find_DR_VER2.py
@@ -10,6 +10,7 @@ import time
 final_list = [] #最終要寫入csv的list
 xml_list = []   #拿來存.xml檔的list
 count = 0
+search_str = '<value>2021-04-' #欲搜尋字串
 
 def xml_read(name_):
     tree = ET.parse(name_)
@@ -17,13 +18,13 @@ def xml_read(name_):
     string1_ = root[0][0][0].text
     string2_ = string1_.split('</')
     for i in string2_:
-        if('<value>2021-04-' in i):                     #尋找xml檔是否有所需要的字串
+        if( search_str in i):                     #尋找xml檔是否有所需要的字串
             tmp_str = i.replace('timeStamp><value>','') #將不要的字串給去除
             tmp_str1 = name_ +","+ tmp_str              #接著將檔名和字串以逗號隔開並且存成一個字串
             result_str = tmp_str1.split(',')            #將剛剛處理的字串分割成一個陣列兩個元素
             final_list.append(result_str)               #放到final
 
-file_list = os.listdir(r'PATH')
+file_list = os.listdir(r'C:\Users\Charles\Documents\202104Apr\nealson\2021-04-08-repush-153\2021-04-08-repush-153')
 #找出目錄底下的所有檔案名稱把它存進file_list裡面
 
 for filename in file_list:
